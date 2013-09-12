102f2c3aa66cefaf98e9608d04838282c9978a11
Jing Quanqi <jingquanqi@gmail.com>
2013-09-12 18:58:30 +0800

    imiao

--- bike/src/main/java/org/quanqi/bike/MainActivity.java
+++ bike/src/main/java/org/quanqi/bike/MainActivity.java
@@ -3,13 +3,17 @@ package org.quanqi.bike;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+        super.onCreate(savedInstanceState);
+        View v = findViewById(R.id.text_main_hello);
+        org.quanqi.argon.anim.BreathAnimation.create(this, v).start();
     }
 
 
